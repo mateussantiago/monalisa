069db5096dad315b189f26e7cc2c096b944b717e
Mateus Santiago <mateus.santiago01@gmail.com>
2019-08-19 12:43:39 -0300

    Spring security adicionado e criptografia da senha
    - Spring security adicionado ao projeto
     - Criptografia da senha  (BCrypt)

--- src/main/java/br/com/monalisa/MonalisaApplication.java
+++ src/main/java/br/com/monalisa/MonalisaApplication.java
@@ -2,12 +2,22 @@ package br.com.monalisa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import sun.security.util.Password;
 
 @SpringBootApplication
 public class MonalisaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonalisaApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
