bb8f21c3256fa2aab5f56d6282dc97a1714df11b
Mateus Santiago <mateus.santiago01@gmail.com>
2019-08-18 23:24:20 -0300

    Arquivos fontes do front end
    - Arquivos fontes das paginas adicionados

--- src/main/java/br/com/monalisa/controller/IndexController.java
+++ src/main/java/br/com/monalisa/controller/IndexController.java
@@ -2,17 +2,18 @@ package br.com.monalisa.controller;
 
 import br.com.monalisa.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
     @Autowired
     private UsuarioService usuarioService;
 
     @RequestMapping("/")
-    public String index() {
-
-        return "Iai, ray ray.";
+    public String index(Model model) {
+        
+        return "index";
     }
 }
