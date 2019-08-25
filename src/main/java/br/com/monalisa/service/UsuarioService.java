8f125e1d55fc0121ee3c398e303c89c486feeb28
Mateus Santiago <mateus.santiago01@gmail.com>
2019-08-25 03:28:59 -0300

    Mudanças scripts sql

--- src/main/java/br/com/monalisa/service/UsuarioService.java
+++ src/main/java/br/com/monalisa/service/UsuarioService.java
@@ -2,12 +2,11 @@ package br.com.monalisa.service;
 
 import br.com.monalisa.dto.UsuarioDTO;
 import br.com.monalisa.model.Usuario;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UsuarioService {
 
     Usuario registrarUsuario(UsuarioDTO usuarioDTO);
 
     Usuario findByLoginAndPassword(String login, String senha);
+
 }
