fd2c9f3301fc59fc22b21322cdaefc19eb413800
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2019-09-01 17:16:36 -0300

    database ta pronto!

--- src/main/resources/database/monalisa.sql
+++ src/main/resources/database/monalisa.sql
@@ -1,4 +1,4 @@
--- DROP SEQUENCE public.seq_id_usuario;
+--DROP SEQUENCE public.seq_id_usuario;
 CREATE SEQUENCE public.seq_id_usuario
    INCREMENT 1
    START 1
@@ -10,14 +10,14 @@ ALTER SEQUENCE public.seq_id_usuario OWNER TO postgres;
 GRANT ALL ON SEQUENCE public.seq_id_usuario TO postgres;
 
 -- Drop table
--- DROP TABLE public.usuario;
+--DROP TABLE public.usuario;
 CREATE TABLE public.usuario (
 	id_usuario int4 NOT NULL,
 	nome varchar(250) NOT NULL,
 	login varchar(50) NOT NULL,
 	email varchar(250) NOT NULL,
 	senha varchar(60) NOT NULL,
-	CONSTRAINT pk_usuario PRIMARY KEY (id_usuario),
+	CONSTRAINT pk_usuario PRIMARY KEY (id_usuario)
 );
 
 -- Permissions
@@ -26,7 +26,7 @@ GRANT ALL ON TABLE public.usuario TO postgres;
 
 ------------------------------------------------------------
 
--- DROP SEQUENCE public.seq_id_tag;
+--DROP SEQUENCE public.seq_id_tag;
 CREATE SEQUENCE public.seq_id_tag
 	INCREMENT BY 1
 	MINVALUE 1
@@ -39,12 +39,12 @@ ALTER SEQUENCE public.seq_id_tag OWNER TO postgres;
 GRANT ALL ON SEQUENCE public.seq_id_tag TO postgres;
 
 -- Drop table
--- DROP TABLE public.tag;
-CREATE TABLE public.tag (
+--DROP TABLE public.tag;
+CREATE TABLE public.tag(
 	id_tag int4 NOT NULL,
 	nome varchar(250) NULL,
 	ativo bool NOT NULL DEFAULT true,
-	CONSTRAINT pk_tag PRIMARY KEY (id_tag),
+	CONSTRAINT pk_tag PRIMARY KEY (id_tag)
 );
 
 -- Permissions
@@ -53,7 +53,7 @@ GRANT ALL ON TABLE public.tag TO postgres;
 
 ------------------------------------------------------------
 
--- DROP SEQUENCE public.seq_id_turma;
+--DROP SEQUENCE public.seq_id_turma;
 CREATE SEQUENCE public.seq_id_turma
 	INCREMENT BY 1
 	MINVALUE 1
@@ -66,7 +66,7 @@ ALTER SEQUENCE public.seq_id_turma OWNER TO postgres;
 GRANT ALL ON SEQUENCE public.seq_id_turma TO postgres;
 
 -- Drop table
--- DROP TABLE public.turma;
+--DROP TABLE public.turma;
 CREATE TABLE public.turma (
 	id_turma int4 NOT NULL,
 	nome varchar(250) NULL,
@@ -81,7 +81,7 @@ GRANT ALL ON TABLE public.turma TO postgres;
 
 ------------------------------------------------------------
 
--- DROP SEQUENCE public.seq_id_postagem;
+--DROP SEQUENCE public.seq_id_postagem;
 CREATE SEQUENCE public.seq_id_postagem
 	INCREMENT BY 1
 	MINVALUE 1
@@ -94,45 +94,20 @@ ALTER SEQUENCE public.seq_id_postagem OWNER TO postgres;
 GRANT ALL ON SEQUENCE public.seq_id_postagem TO postgres;
 
 -- Drop table
--- DROP TABLE public.postagem;
+--DROP TABLE public.postagem;
 CREATE TABLE public.postagem (
 	id_postagem int4 NOT NULL,
 	ativo bool NOT NULL DEFAULT true,
-	conteudo varchar(5000) NULL,
+	conteudo varchar(5000) NOT NULL,
 	avaliacao_positiva int4 NULL,
 	avaliacao_negativa int4 NULL,
-	CONSTRAINT pk_postagem PRIMARY KEY (id_postagem)
+	id_postagem_genitora int4 NULL,
+
+	CONSTRAINT pk_postagem PRIMARY KEY (id_postagem),
+
+	constraint fk_postagem foreign key (id_postagem)
+	references public.postagem (id_postagem)
 );
-
--- Permissions
-ALTER TABLE public.postagem OWNER TO postgres;
-GRANT ALL ON TABLE public.postagem TO postgres;
-
-------------------------------------------------------------
-
--- DROP SEQUENCE public.seq_id_postagem;
-CREATE SEQUENCE public.seq_id_postagem
-	INCREMENT BY 1
-	MINVALUE 1
-	MAXVALUE 999999999999999999
-	CACHE 1
-	NO CYCLE;
--- Permissions
-ALTER SEQUENCE public.seq_id_postagem OWNER TO postgres;
-GRANT ALL ON SEQUENCE public.seq_id_postagem TO postgres;
--- Drop table
--- DROP TABLE public.postagem;
-CREATE TABLE public.postagem (
-	id_postagem int4 NOT NULL,
-	ativo bool NOT NULL DEFAULT true,
-	conteudo varchar(5000) NULL,
-	avaliacao_positiva int4 NULL,
-	avaliacao_negativa int4 NULL,
-	CONSTRAINT pk_postagem PRIMARY KEY (id_postagem)
-);
--- Permissions
-ALTER TABLE public.postagem OWNER TO postgres;
-GRANT ALL ON TABLE public.postagem TO postgres;
 
 ------------------------------------------------------------
 
@@ -183,16 +158,16 @@ GRANT ALL ON SEQUENCE public.seq_id_turma_usuario TO postgres;
 -- DROP TABLE public.turmausuario;
 CREATE TABLE public.turmausuario(
     id_turma_usuario int4 NOT NULL,
-    id_usario int4 NOT NULL,
+    id_usuario int4 NOT NULL,
     id_turma int4 NOT NULL,
     data_inicio TIMESTAMP,
     data_fim TIMESTAMP,
     CONSTRAINT pk_turmausuario PRIMARY KEY (id_turma_usuario),
 
-    CONSTRAINT fk_usuario FOREIGN KEY (id_usuario),
+    CONSTRAINT fk_usuario FOREIGN KEY (id_usuario)
     REFERENCES public.usuario (id_usuario),
 
-    CONSTRAINT fk_turma FOREIGN KEY (id_turma),
+    CONSTRAINT fk_turma FOREIGN KEY (id_turma)
     REFERENCES public.turma (id_turma)
 );
 
@@ -246,7 +221,7 @@ GRANT ALL ON SEQUENCE public.seq_id_assunto_turma TO postgres;
 -- Drop table
 -- DROP TABLE public.assuntoturma;
 CREATE TABLE public.assuntoturma(
-
+	id_assuntoturma int4 not null,
     id_assunto int4 NOT NULL,
     id_turma int4 NOT NULL,
     ativo bool NOT NULL,
@@ -255,7 +230,9 @@ CREATE TABLE public.assuntoturma(
     REFERENCES public.assunto(id_assunto),
 
     CONSTRAINT fk_turma FOREIGN KEY (id_turma)
-    REFERENCES public.turma(id_turma)
+    REFERENCES public.turma(id_turma),
+
+    constraint pk_assuntoturma primary key (id_assuntoturma)
 );
 
 -- Permissions
@@ -290,7 +267,9 @@ CREATE TABLE public.assuntoturmapostagem(
     REFERENCES public.assuntoturma(id_assuntoturma),
 
     CONSTRAINT fk_postagem FOREIGN KEY (id_postagem)
-    REFERENCES public.postagem(id_postagem)
+    REFERENCES public.postagem(id_postagem),
+
+    CONSTRAINT pk_assuntoturmapostagem PRIMARY KEY (id_assuntoturma_postagem)
 );
 
 -- Permissions
