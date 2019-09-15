5dd409868dccc0ca556734a52814c9a03e9af0da
Mateus Santiago <mateus.santiago01@gmail.com>
2019-09-15 18:52:55 -0300

    Adicionando algumas correcos nos controllers

--- src/main/java/br/com/monalisa/repository/AssuntoTurmaRepository.java
+++ src/main/java/br/com/monalisa/repository/AssuntoTurmaRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AssuntoTurmaRepository   extends JpaRepository<AssuntoTurma, Long>, JpaSpecificationExecutor<AssuntoTurma> {
+public interface AssuntoTurmaRepository extends JpaRepository<AssuntoTurma, Long>, JpaSpecificationExecutor<AssuntoTurma> {
     AssuntoTurma findAssuntoTurmaByAssuntoAndTurma(Long assunto, Long turma);
 }
