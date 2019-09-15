2c40072d92c827e719a8dbe63691ac417b1a3bc3
Maria Rayane Alves <mrayalves05@gmail.com>
2019-09-15 20:53:40 -0300

    Modificando os metodos no repository de assunto para considerar o atributo de exclusao logica nas buscas

--- src/main/java/br/com/monalisa/service/AssuntoService.java
+++ src/main/java/br/com/monalisa/service/AssuntoService.java
@@ -14,15 +14,15 @@ public class AssuntoService {
     @Autowired
     private AssuntoRepository assuntoRepository;
 
+    public List<Assunto> buscarTodos() {
+        return assuntoRepository.buscarTodos();
+    }
+
+    public Assunto buscarPorId(Long id) {
+        return assuntoRepository.buscarPorId(id);
+    }
+
     public Assunto salvar(Assunto assunto) {
         return assuntoRepository.save(assunto);
-    }
-
-    public List<Assunto> buscarTodos() {
-        return assuntoRepository.findAll();
-    }
-
-    public Optional<Assunto> buscarPorId(Long id) {
-        return assuntoRepository.findById(id);
     }
 }
