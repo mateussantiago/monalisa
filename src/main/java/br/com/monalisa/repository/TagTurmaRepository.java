8409313004bf592c0d7488512a55d5a696e7af01
Maria Rayane Alves <mrayalves05@gmail.com>
2019-09-15 21:07:49 -0300

    Modificando os metodos no repository de tagturma para considerar o atributo de exclusao logica nas buscas

--- src/main/java/br/com/monalisa/repository/TagTurmaRepository.java
+++ src/main/java/br/com/monalisa/repository/TagTurmaRepository.java
@@ -3,8 +3,17 @@ package br.com.monalisa.repository;
 import br.com.monalisa.model.TagTurma;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TagTurmaRepository extends JpaRepository<TagTurma, Long>, JpaSpecificationExecutor<TagTurma> {
+    @Query(value = "select * from public.tag_turma where ativo is true", nativeQuery = true)
+    List<TagTurma> buscarTodos();
+
+    @Query(value = "select * from public.tag_turma where id_tag_turma = :id and ativo is true", nativeQuery = true)
+    TagTurma buscarPorId(@Param(value = "id") Long id);
 }
--- src/main/java/br/com/monalisa/service/TagTurmaService.java
+++ src/main/java/br/com/monalisa/service/TagTurmaService.java
@@ -6,22 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class TagTurmaService {
     @Autowired
     private TagTurmaRepository tagTurmaRepository;
 
+    public List<TagTurma> buscarTodos() {
+        return tagTurmaRepository.buscarTodos();
+    }
+
+    public TagTurma buscarPorId(Long id) {
+        return tagTurmaRepository.buscarPorId(id);
+    }
+
     public TagTurma salvar(TagTurma turma) {
         return tagTurmaRepository.save(turma);
-    }
-
-    public List<TagTurma> buscarTodos() {
-        return tagTurmaRepository.findAll();
-    }
-
-    public Optional<TagTurma> buscarPorId(Long id) {
-        return tagTurmaRepository.findById(id);
     }
 }
